e6e11d1519f4f4b286843aafa40132b7a94f4cfd
FantasyTeddy <matthiasbaer@bluewin.ch>
2018-07-26 10:37:00 +0200

    Release version 2.0.15

--- src/main/java/jotato/quantumflux/QuantumFluxMod.java
+++ src/main/java/jotato/quantumflux/QuantumFluxMod.java
@@ -19,7 +19,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class QuantumFluxMod {
 	public static final String MODID = "quantumflux";
 	public static final String MODNAME = "QuantumFlux";
-	public static final String VERSION = "2.0.14";
+	public static final String VERSION = "2.0.15";
 	public static final String DEPENDENCIES = "required-after:redstoneflux";
 	public static final String TEXTURE_BASE = MODID + ":";
 
